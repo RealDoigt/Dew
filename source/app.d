b0a8526f05fecd44e8dca584c1e6aedc2849b500
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-09-27 18:44:58 -0400

    stupid that doesn't work but needs to be added because its better than nothing

--- source/app.d
+++ source/app.d
@@ -1,34 +1,12 @@
-import token_class;
 import std.stdio;
 import std.file;
-import scanner;
 import std.string;
-
-auto tokenize (string source)
-{
-    auto scanner = new Scanner(source);
-    return scanner.scanTokens;
-}
-
-auto tokenizeFile(string path)
-{
-    scope (failure)
-        "the system encountered a problem with the file".writeln;
-
-    return path.readText.tokenize;
-}
 
 void main(string[] args)
 {
     if (args.length >= 2)
     {
-        Token[] tokens;
-        
-        for (size_t i = 1; i < args.length; ++i) 
-            tokens ~= args[i].tokenizeFile;
-            
-        foreach (t; tokens) t.writeln;
     }
-    
+
     else "usage: dew <file(s)>".writeln;
 }
