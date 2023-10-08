f2352d7447c6e3fe55d5269f0813f7f57666feba
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-10-07 20:33:21 -0400

    file scanner

--- source/app.d
+++ source/app.d
@@ -1,12 +1,23 @@
 import std.stdio;
 import std.file;
 import std.string;
+import std.array;
 
 void main(string[] args)
 {
+    string[string] dSourceFiles, dewSourceFiles;
+
     if (args.length >= 2)
     {
         foreach (file; args[1..$-1])
+        {
+            if (file.exists)
+            {
+                if (file.endsWith(".d")) dSourceFiles[file] = file.readText;
+                else if (file.endsWith(".dew")) dewSourceFiles[file] = file.readText;
+                else "Unrecognised file type of %d. Use only D and Dew source files.".writefln(file);
+            }
+        }
     }
 
     else "usage: dew <file(s)>".writeln;
